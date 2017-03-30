0a6a6189455c377587d13a04b57cc6de85d77c8c
Lauren <Lauren.Wang@nordstrom.com>
2017-03-29 17:20:17 -0700

    some cleanup and handling eventId

--- Lesson2_CreateViewWithEventConsumer/winner-view/winner.js
+++ Lesson2_CreateViewWithEventConsumer/winner-view/winner.js
@@ -2,9 +2,10 @@
 
 const AJV = require('ajv')
 const aws = require('aws-sdk') // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
+const bigInt = require('big-integer')
 
 // TODO Get these from a better place later
-const eventSchema = require('./retail-stream-schema-ingress.json')
+const eventSchema = require('./retail-stream-schema-egress.json')
 const productCreateSchema = require('./product-create-schema.json')
 const productImageSchema = require('./product-image-schema.json')
 const productPurchaseSchema = require('./product-purchase-schema.json')
@@ -29,8 +30,9 @@ const constants = {
   // self
   MODULE: 'winner-view/winner.js',
   // methods
-  METHOD_UPDATE_CONTRIBUTIONS_TABLES: 'updateContributionsTable',
+  METHOD_RECORD_CONTRIBUTION: 'recordContribution',
   METHOD_UPDATE_SCORES_TABLES: 'updateScoresTable',
+  METHOD_VERIFY_NEW_PURCHASE: 'verifyNewPurchase',
   METHOD_PROCESS_EVENT: 'processEvent',
   METHOD_PROCESS_KINESIS_EVENT: 'processKinesisEvent',
   // errors
@@ -44,9 +46,9 @@ const impl = {
   /**
    * Update contributions tables.  Example event:
    * {
-   *   "schema": "com.nordstrom/retail-stream-ingress/1-0-0",
-   *   "origin": "hello-retail/product-producer-creator/uniqueId/friendlyName
-   *   "timeOrigin": "2017-01-12T18:29:25.171Z",
+   *   "schema": "com.nordstrom/retail-stream-egress/1-0-0",
+   *   "origin": "hello-retail/product-producer-creator/uniqueId/friendlyName",
+   *   "timeOrigin": "2017-03-28T23:29:23.160Z",
    *   "data": {
    *     "schema": "com.nordstrom/product/create/1-0-0",
    *     "id": "4579874",
@@ -54,12 +56,15 @@ const impl = {
    *     "name": "Polo Ralph Lauren 3-Pack Socks",
    *     "description": "PAGE:/s/polo-ralph-lauren-3-pack-socks/4579874",
    *     "category": "Socks for Men"
-   *   }
+   *   },
+   *   "eventId":"shardId-000000000002:49571669009522853278119462494300940056686602130905104418",
+   *   "timeIngest":"2017-03-28T23:29:23.262Z",
+   *   "timeProcess":"2017-03-28T23:29:29.720Z"
    * }
    * @param event Either a product/create or a product/image event.
    * @param complete The callback to inform of completion, with optional error parameter.
    */
-  updateContributionsTable: (role, event, complete) => {
+  recordContribution: (role, event, complete) => {
     const updated = Date.now()
 
     let priorErr
@@ -71,14 +76,44 @@ const impl = {
           priorErr = false
         }
       } else if (priorErr && err) { // second update result, if an error was previously received and we have a new one
-        complete(`${constants.METHOD_UPDATE_CONTRIBUTIONS_TABLES} - errors updating DynamoDb: ${[priorErr, err]}`)
+        complete(`${constants.METHOD_RECORD_CONTRIBUTION} - errors updating DynamoDb: ${[priorErr, err]}`)
       } else if (priorErr || err) {
-        complete(`${constants.METHOD_UPDATE_CONTRIBUTIONS_TABLES} - error updating DynamoDb: ${priorErr || err}`)
+        complete(`${constants.METHOD_RECORD_CONTRIBUTION} - error updating DynamoDb: ${priorErr || err}`)
       } else { // second update result if error was not previously seen
         complete()
       }
     }
 
+    // Initialize row in Scores table for this contributor
+    const dbParamsScores = {
+      TableName: constants.TABLE_SCORES_NAME,
+      Key: {
+        userId: event.origin,
+        role,
+      },
+      UpdateExpression: [
+        'set',
+        '#c=if_not_exists(#c,:c),',
+        '#cb=if_not_exists(#cb,:cb),',
+        '#sc=if_not_exists(#sc, :num)',
+      ].join(' '),
+      ExpressionAttributeNames: {
+        '#c': 'created',
+        '#cb': 'createdBy',
+        '#sc': 'score',
+      },
+      ExpressionAttributeValues: {
+        ':c': updated,
+        ':cb': event.origin,
+        ':num': 0,
+      },
+      ReturnValues: 'NONE',
+      ReturnConsumedCapacity: 'NONE',
+      ReturnItemCollectionMetrics: 'NONE',
+    }
+    dynamo.update(dbParamsScores, updateCallback)
+
+    // Record product's contributor
     const expression = [
       'set',
       '#c=if_not_exists(#c,:c),',
@@ -86,86 +121,24 @@ const impl = {
       '#u=:u,',
       '#ub=:ub,',
     ]
+    if (role === 'creator') {
+      expression.push('#ag=if_not_exists(#ag,:ag)')
+    } else if (role === 'photographer') {
+      expression.push('#ag=:ag')
+    }
     const attNames = {
       '#c': 'created',
       '#cb': 'createdBy',
       '#u': 'updated',
       '#ub': 'updatedBy',
+      '#ag': role,
     }
     const attValues = {
       ':c': updated,
       ':cb': event.origin,
       ':u': updated,
       ':ub': event.origin,
-    }
-    if (role === 'creator') {
-      expression.push('#cr=if_not_exists(#cr,:cr)')
-      attNames['#cr'] = 'creator'
-      attValues[':cr'] = event.origin
-
-      // TODO would be more efficient to do this once on login than every time, but need to write a login method and
-      // look for a login schema, etc
-      const dbParamsScores = {
-        TableName: constants.TABLE_SCORES_NAME,
-        Key: {
-          userId: event.origin,
-          role: 'creator',
-        },
-        UpdateExpression: [
-          'set',
-          '#c=if_not_exists(#c,:c),',
-          '#cb=if_not_exists(#cb,:cb),',
-          '#sc=if_not_exists(#sc, :num)',
-        ].join(' '),
-        ExpressionAttributeNames: {
-          '#c': 'created',
-          '#cb': 'createdBy',
-          '#sc': 'score',
-        },
-        ExpressionAttributeValues: {
-          ':c': updated,
-          ':cb': event.origin,
-          ':num': 0,
-        },
-        ReturnValues: 'NONE',
-        ReturnConsumedCapacity: 'NONE',
-        ReturnItemCollectionMetrics: 'NONE',
-      }
-      dynamo.update(dbParamsScores, updateCallback)
-    } else if (role === 'photographer') {
-      expression.push('#ph=:ph')
-      attNames['#ph'] = 'photographer'
-      attValues[':ph'] = event.origin
-
-      // TODO would be more efficient to do this once on login than every time, but need to write a login method and
-      // look for a login schema, etc
-      const dbParamsScores = {
-        TableName: constants.TABLE_SCORES_NAME,
-        Key: {
-          userId: event.origin,
-          role: 'photographer',
-        },
-        UpdateExpression: [
-          'set',
-          '#c=if_not_exists(#c,:c),',
-          '#cb=if_not_exists(#cb,:cb),',
-          '#sc=if_not_exists(#sc, :num)',
-        ].join(' '),
-        ExpressionAttributeNames: {
-          '#c': 'created',
-          '#cb': 'createdBy',
-          '#sc': 'score',
-        },
-        ExpressionAttributeValues: {
-          ':c': updated,
-          ':cb': event.origin,
-          ':num': 0,
-        },
-        ReturnValues: 'NONE',
-        ReturnConsumedCapacity: 'NONE',
-        ReturnItemCollectionMetrics: 'NONE',
-      }
-      dynamo.update(dbParamsScores, updateCallback)
+      ':ag': event.origin,
     }
 
     const dbParamsContributions = {
@@ -185,18 +158,22 @@ const impl = {
   /**
    * Update scores table.  Example event:
    * {
-   *   "schema": "com.nordstrom/retail-stream-ingress/1-0-0",
-   *   "origin": "hello-retail/product-purchaser/uniqueId/friendlyName
-   *   "timeOrigin": "2017-01-12T18:29:25.171Z",
-   *   "data": {
-   *     "schema": "com.nordstrom/product/purchase/1-0-0",
-   *     "id": "4579874"
-   *   }
-   * }
+   *   "schema":"com.nordstrom/retail-stream-egress/1-0-0",
+   *   "timeOrigin":"2017-03-28T23:52:53.763Z",
+   *   "data":{
+   *      "schema":"com.nordstrom/product/purchase/1-0-0",
+   *      "id":"7749361"
+   *   },
+   *   "origin":"hello-retail/web-client-purchase-product/uniqueId/friendlyName",
+   *   "eventId":"shardId-000000000001:49571669109051079099161633575187621651768511161306185746",
+   *   "timeIngest":"2017-03-28T23:52:53.818Z",
+   *   "timeProcess":"2017-03-28T23:52:59.677Z"
+   * },
+   * @param data The data record retrieved from the Contributions table to know whom to credit.
    * @param event The purchase event.
    * @param complete The callback to inform of completion, with optional error parameter.
    */
-  updateScoresTable: (event, complete) => {
+  updateScoresTable: (data, event, complete) => {
     const updated = Date.now()
 
     let priorErr
@@ -215,7 +192,144 @@ const impl = {
         complete()
       }
     }
+    if (!data || (!data.creator && !data.photographer)) {
+      console.log(`No contributor information for product ${event.data.id}, so no effect on scores.`)
+      complete()
+    } else {
+      if (data.creator) {
+        const dbParamsCreator = {
+          TableName: constants.TABLE_SCORES_NAME,
+          Key: {
+            userId: data.creator,
+            role: 'creator',
+          },
+          UpdateExpression: [
+            'set',
+            '#u=:u,',
+            '#ub=:ub,',
+            '#sc=#sc + :num',
+          ].join(' '),
+          ExpressionAttributeNames: {
+            '#u': 'updated',
+            '#ub': 'updatedBy',
+            '#sc': 'score',
+          },
+          ExpressionAttributeValues: {
+            ':u': updated,
+            ':ub': event.origin,
+            ':num': 1,
+          },
+          ReturnValues: 'NONE',
+          ReturnConsumedCapacity: 'NONE',
+          ReturnItemCollectionMetrics: 'NONE',
+        }
+        dynamo.update(dbParamsCreator, updateCallback)
+      } else {
+        updateCallback()
+      }
+      if (data.photographer) {
+        const dbParamsPhotographer = {
+          TableName: constants.TABLE_SCORES_NAME,
+          Key: {
+            userId: data.photographer,
+            role: 'photographer',
+          },
+          UpdateExpression: [
+            'set',
+            '#u=:u,',
+            '#ub=:ub,',
+            '#sc=#sc + :num',
+          ].join(' '),
+          ExpressionAttributeNames: {
+            '#u': 'updated',
+            '#ub': 'updatedBy',
+            '#sc': 'score',
+          },
+          ExpressionAttributeValues: {
+            ':u': updated,
+            ':ub': event.origin,
+            ':num': 1,
+          },
+          ReturnValues: 'NONE',
+          ReturnConsumedCapacity: 'NONE',
+          ReturnItemCollectionMetrics: 'NONE',
+        }
+        dynamo.update(dbParamsPhotographer, updateCallback)
+      } else {
+        updateCallback()
+      }
+    }
+  },
+  /**
+   * Log latest purchase of a given product id to the Contributions table, creating the product record, if needed.
+   * (This is in case the fanout doesn't connect till the create event has passed.)
+   * @param id The product id.
+   * @param sequenceNumber The sequence number from the event that is currently being processed.
+   * @param origin The generator of the event (for logging the source of the update).
+   * @param complete The callback to inform of completion, with optional error parameter.
+   */
+  updateSequenceNumber: (id, sequenceNumber, origin, complete) => {
+    const updated = Date.now()
 
+    // Record latest sequence number
+    const expression = [
+      'set',
+      '#c=if_not_exists(#c,:c),',
+      '#cb=if_not_exists(#cb,:cb),',
+      '#u=:u,',
+      '#ub=:ub,',
+      '#ag=:ag',
+    ]
+    const attNames = {
+      '#c': 'created',
+      '#cb': 'createdBy',
+      '#u': 'updated',
+      '#ub': 'updatedBy',
+      '#ag': 'lastSequenceNumber',
+    }
+    const attValues = {
+      ':c': updated,
+      ':cb': origin,
+      ':u': updated,
+      ':ub': origin,
+      ':ag': sequenceNumber,
+    }
+
+    const dbParamsContributions = {
+      TableName: constants.TABLE_CONTRIBUTIONS_NAME,
+      Key: {
+        productId: id,
+      },
+      UpdateExpression: expression.join(' '),
+      ExpressionAttributeNames: attNames,
+      ExpressionAttributeValues: attValues,
+      ReturnValues: 'NONE',
+      ReturnConsumedCapacity: 'NONE',
+      ReturnItemCollectionMetrics: 'NONE',
+    }
+    dynamo.update(dbParamsContributions, complete)
+  },
+  verifyNewPurchase: (event, complete) => {
+    let priorErr
+    const updateCallback = (err) => {
+      if (priorErr === undefined) { // first update result
+        if (err) {
+          priorErr = err
+        } else {
+          priorErr = false
+        }
+      } else if (priorErr && err) { // second update result, if an error was previously received and we have a new one
+        complete(`${constants.METHOD_VERIFY_NEW_PURCHASE} - errors updating DynamoDb: ${[priorErr, err]}`)
+      } else if (priorErr || err) {
+        complete(`${constants.METHOD_VERIFY_NEW_PURCHASE} - error updating DynamoDb: ${priorErr || err}`)
+      } else { // second update result if error was not previously seen
+        complete()
+      }
+    }
+
+    const sequenceNumber = event.eventId.split(':')[1]
+    console.log('event seq number', sequenceNumber) // TODO remove
+    console.log('verify for product id', event.data.id) // TODO remove
     const dbParamsContributions = {
       Key: {
         productId: event.data.id,
@@ -224,79 +338,20 @@ const impl = {
       AttributesToGet: [
         'creator',
         'photographer',
+        'lastSequenceNumber',
       ],
       ConsistentRead: false,
       ReturnConsumedCapacity: 'NONE',
     }
     dynamo.get(dbParamsContributions, (err, data) => {
       if (err) {
-        complete(`${constants.METHOD_UPDATE_SCORES_TABLES} - errors getting product ${event.data.id} from DynamoDb table ${constants.TABLE_CONTRIBUTIONS_NAME}: ${err}`)
-      } else if (!data.Item || (!data.Item.creator && !data.Item.photographer)) {
-        console.log(`No contributor information for product ${event.data.id}, so no effect on scores.`)
+        complete(`${constants.METHOD_VERIFY_NEW_PURCHASE} - errors getting product ${event.data.id} from DynamoDb table ${constants.TABLE_CONTRIBUTIONS_NAME}: ${err}`)
+      } else if (data.Item && data.Item.lastSequenceNumber && bigInt(data.Item.lastSequenceNumber).compare(bigInt(sequenceNumber)) >= 0) {
+        console.log(`Event processing has already moved to ${data.Item.lastSequenceNumber} for product ${event.data.id}, so discarding.`)
         complete()
-      } else {
-        if (data.Item.creator) {
-          const dbParamsCreator = {
-            TableName: constants.TABLE_SCORES_NAME,
-            Key: {
-              userId: data.Item.creator,
-              role: 'creator',
-            },
-            UpdateExpression: [
-              'set',
-              '#u=:u,',
-              '#ub=:ub,',
-              '#sc=#sc + :num',
-            ].join(' '),
-            ExpressionAttributeNames: {
-              '#u': 'updated',
-              '#ub': 'updatedBy',
-              '#sc': 'score',
-            },
-            ExpressionAttributeValues: {
-              ':u': updated,
-              ':ub': event.origin,
-              ':num': 1,
-            },
-            ReturnValues: 'NONE',
-            ReturnConsumedCapacity: 'NONE',
-            ReturnItemCollectionMetrics: 'NONE',
-          }
-          dynamo.update(dbParamsCreator, updateCallback)
-        } else {
-          updateCallback()
-        }
-        if (data.Item.photographer) {
-          const dbParamsPhotographer = {
-            TableName: constants.TABLE_SCORES_NAME,
-            Key: {
-              userId: data.Item.photographer,
-              role: 'photographer',
-            },
-            UpdateExpression: [
-              'set',
-              '#u=:u,',
-              '#ub=:ub,',
-              '#sc=#sc + :num',
-            ].join(' '),
-            ExpressionAttributeNames: {
-              '#u': 'updated',
-              '#ub': 'updatedBy',
-              '#sc': 'score',
-            },
-            ExpressionAttributeValues: {
-              ':u': updated,
-              ':ub': event.origin,
-              ':num': 1,
-            },
-            ReturnValues: 'NONE',
-            ReturnConsumedCapacity: 'NONE',
-            ReturnItemCollectionMetrics: 'NONE',
-          }
-          dynamo.update(dbParamsPhotographer, updateCallback)
-        } else {
-          updateCallback()
-        }
+      } else { // !data.Item || !data.Item.lastSequenceNumber || is the latest, then just  and update scores table
+        impl.updateScoresTable(data.Item, event, updateCallback)
+        impl.updateSequenceNumber(event.data.id, sequenceNumber, event.origin, updateCallback)
       }
     })
   },
@@ -316,19 +371,19 @@ const impl = {
       if (!ajv.validate(productCreateSchemaId, event.data)) {
         complete(`${constants.METHOD_PROCESS_EVENT} ${constants.BAD_MSG} could not validate event to '${productCreateSchema}' schema. Errors: ${ajv.errorsText()}`)
       } else {
-        impl.updateContributionsTable('creator', event, complete)
+        impl.recordContribution('creator', event, complete)
       }
     } else if (event.data.schema === productImageSchemaId) {
       if (!ajv.validate(productImageSchemaId, event.data)) {
         complete(`${constants.METHOD_PROCESS_EVENT} ${constants.BAD_MSG} could not validate event to '${productImageSchema}' schema. Errors: ${ajv.errorsText()}`)
       } else {
-        impl.updateContributionsTable('photographer', event, complete)
+        impl.recordContribution('photographer', event, complete)
       }
     } else if (event.data.schema === productPurchaseSchemaId) {
       if (!ajv.validate(productPurchaseSchemaId, event.data)) {
         complete(`${constants.METHOD_PROCESS_EVENT} ${constants.BAD_MSG} could not validate event to '${productPurchaseSchema}' schema. Errors: ${ajv.errorsText()}`)
       } else {
-        impl.updateScoresTable(event, complete)
+        impl.verifyNewPurchase(event, complete)
       }
     } else {
       // TODO remove console.log and pass the above message once we are only receiving subscribed events
@@ -392,11 +447,12 @@ module.exports = {
         let successes = 0
         const complete = (err) => {
           if (err) {
+            console.log(err)
             // TODO uncomment following
             // throw new Error(`${constants.MODULE} ${err}`);
             // TODO remove rest of block to use above.
             const msg = `${constants.MODULE} ${err}`
-            if (err.indexOf(`${constants.MODULE} ${constants.METHOD_PROCESS_EVENT}  ${constants.BAD_MSG}`) !== -1) {
+            if (msg.indexOf(`${constants.MODULE} ${constants.METHOD_PROCESS_EVENT} ${constants.BAD_MSG}`) !== -1) {
               console.log('######################################################################################')
               console.log(msg)
               console.log('######################################################################################')
@@ -406,10 +462,10 @@ module.exports = {
             }
           } else {
             successes += 1
-            if (successes === kinesisEvent.Records.length) {
-              console.log(`${constants.MODULE} ${constants.METHOD_PROCESS_KINESIS_EVENT} - all ${kinesisEvent.Records.length} events processed successfully.`)
-              callback(null, true)
-            }
+          }
+          if (successes === kinesisEvent.Records.length) {
+            console.log(`${constants.MODULE} ${constants.METHOD_PROCESS_KINESIS_EVENT} - all ${kinesisEvent.Records.length} events processed successfully.`)
+            callback()
           }
         }
         for (let i = 0; i < kinesisEvent.Records.length; i++) {
